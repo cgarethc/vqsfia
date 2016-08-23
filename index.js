82c51dbe84e5d56e3e522b3bdd4074e34d66d687
Gareth Cronin <gareth@cronin.co.nz>
2016-08-23 15:35:25 +1200

    bug fixes, column widths corrected

--- index.js
+++ index.js
@@ -3,13 +3,16 @@
 (function(){
 'use strict';
 
+// deps
 const officegen = require('officegen');
 const log4js = require('log4js');
 const fs = require('fs');
 const parse = require('csv-parse');
 const commandline = require('commandline-parser');
 
-const FIRST_COLUMN = 3;
+// column to start reading the CSV from (zero-based)
+const FIRST_COLUMN = 2;
+// source CSV file
 const MATRIX_CSV = 'skillsmatrix_v2.1.csv';
 
 // configure logger
@@ -42,7 +45,7 @@ clparser.addArgument('level' ,{
 });
 clparser.addArgument('progression' ,{
 	flags : ['p','progession'],
-	desc : 'specify a level to progress to',
+	desc : 'specify a level to progress to, if not provided a skills profile will be produced',
 	optional : true,
 });
 clparser.exec();
@@ -50,31 +53,53 @@ clparser.exec();
 let docx = officegen( 'docx' );
 
 let parser = parse();
-// Use the writable stream api
 let header;
 let fileName;
-let table = [];
-parser.on('data', function(row){
-  if(!header){
-    header = row;
-    logger.debug(header);
-    let paragraph = docx.createP();
-    paragraph.addText ('Skills Profile', {font_size: 24, bold: true});
-
-  }
-  if((row[0] === clparser.get('role') && row[1] === clparser.get('level')) && !clparser.get('progression')){
-    logger.debug(row);
-    let paragraph = docx.createP();
-    paragraph.addText (`${row[0]} - ${row[1]}`, {font_size: 12, bold: true});
-    fileName = `Skills Profile ${row[0]} - ${row[1]}.docx`;
-
-    for(var counter = FIRST_COLUMN; counter < row.length; counter++){
-      if(row[counter] !== 'N/A'){
-        table.push([header[counter], row[counter]]);
+// construct the table header, setting the column widths in pixels
+let table = [
+  [
+    {
+      val:'Skill',
+      opts: {
+        b:true,
+        cellColWidth: 800
+      }
+    },
+    {
+      val:'Requirement',
+      opts: {
+        b:true,
+        cellColWidth: 6000
       }
     }
-  }
-});
+  ]
+  ];
+
+  // parse
+  parser.on('data', function(row){
+    if(!header){
+      // read the CSV header
+      header = row;
+      logger.debug(header);
+
+      // output a heading to the document
+      let paragraph = docx.createP();
+      paragraph.addText ('Skills Profile', {font_size: 24, bold: true});
+    }
+    else if((row[0] === clparser.get('role') && row[1] === clparser.get('level')) && !clparser.get('progression')){
+      // create a skills profile table row for the matching role and level
+      logger.debug(row);
+      let paragraph = docx.createP();
+      paragraph.addText (`${row[0]} - ${row[1]}`, {font_size: 12, bold: true});
+      fileName = `Skills Profile ${row[0]} - ${row[1]}.docx`;
+
+      for(var counter = FIRST_COLUMN; counter < row.length; counter++){
+        if(row[counter] !== 'N/A'){
+          table.push([header[counter], row[counter]]);
+        }
+      }
+    }
+  });
 
 // Catch any error
 parser.on('error', function(err){
@@ -86,7 +111,6 @@ parser.on('finish', function(){
   let out = fs.createWriteStream(fileName);
 
   let tableStyle = {
-    tableColWidth: 4261,
     tableSize: 24,
     tableFontFamily: "Arial",
     tableAlign: "left",
